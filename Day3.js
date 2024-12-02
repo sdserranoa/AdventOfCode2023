02a010b4b62cec81acc01d882e64979bebeb2d03
Santiago Serrano <serranoayz@gmail.com>
2024-12-01 19:19:29 -0500

    Day3 solved

--- Day3.js
+++ Day3.js
@@ -5,10 +5,8 @@ let sum = 0
 
 let matrix = data.split('\r\n')
 
-
 //i=y
 //j=x
-
 //k=y
 //l=x
 for (let i = 0; i < matrix.length; i++) {
@@ -29,7 +27,6 @@ for (let i = 0; i < matrix.length; i++) {
 
         } else {
             if (addNumber) {
-                //console.log(num)
                 sum += Number(num)
                 addNumber = false
             }
@@ -43,6 +40,78 @@ for (let i = 0; i < matrix.length; i++) {
 
 }
 
-console.log("sum", sum);
+//console.log("sum", sum);
 
 //For second part add a hashmap that stores the numbers and the position of the gear
+sum = 0
+
+let map = new Map()
+
+
+for (let i = 0; i < matrix.length; i++) {
+    let num = ''
+    let addNumber = false
+    let gearX = 0
+    let gearY = 0
+    for (let j = 0; j < matrix[i].length; j++) {
+        if (matrix[i][j].match(/[0-9]/g)) {
+            num += matrix[i][j]
+            for (let k = i - 1; k <= i + 1; k++) {
+                for (let l = j - 1; l <= j + 1; l++) {
+                    if (k > -1 && k < matrix.length && l > -1 && l < matrix[i].length) {
+                        if (matrix[k][l].match(/[*]/g)) {
+                            addNumber = true
+                            gearX = k
+                            gearY = l
+                        }
+                    }
+                }
+            }
+
+        } else {
+            if (addNumber) {
+                if (map.has([gearX, gearY] + '')) {
+                    let currentArr = map.get([gearX, gearY] + '')
+                    currentArr.push(num)
+                    map.set([gearX, gearY] + '', currentArr)
+                } else {
+                    let newArr = []
+                    newArr.push(num)
+                    map.set([gearX, gearY] + '', newArr)
+                }
+                sum += Number(num)
+                addNumber = false
+            }
+            num = ''
+            gearX = 0
+            gearY = 0
+        }
+    }
+    if (num && addNumber) {
+        if (map.has([gearX, gearY] + '')) {
+            let currentArr = map.get([gearX, gearY] + '')
+            currentArr.push(num)
+            map.set([gearX, gearY] + '', currentArr)
+        } else {
+            let newArr = []
+            newArr.push(num)
+            map.set([gearX, gearY] + '', newArr)
+        }
+        sum += Number(num)
+        addNumber = false
+    }
+
+}
+sum = 0
+
+map.forEach((gear) => {
+    if (gear.length === 2) {
+        sum += Number(gear[0]) * Number(gear[1])
+    }
+})
+console.log(map);
+
+console.log("sum", sum);
+
+
+//console.log("map",map);
--- README.md
+++ README.md
@@ -7,12 +7,18 @@ This is my try for 2023
    Don't make extra operation that solve a problem that does not exist
 2. Lesson 2
    Simplicity ofter is better without time contraints
-3. Lesson 2
+3. Lesson 3
 
    Consider edge cases!!
+4. Lesson 4
+
+   \
+
+
 
 - [x] Day1
 - [x] Day2
-- [ ] Day3
+- [x] Day3
+- [ ] Day4
 
 
